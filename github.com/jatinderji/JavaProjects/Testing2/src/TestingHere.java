6d5a4abe63d12af3e114a3e6dbdf74af177e1e7b
student <student@DESKTOP-C67GM0L>
2019-02-06 15:48:11 +0530

    Second_2

--- github.com/jatinderji/JavaProjects/Testing2/src/TestingHere.java
+++ github.com/jatinderji/JavaProjects/Testing2/src/TestingHere.java
@@ -1,7 +1,7 @@
 
 public class TestingHere {
 	public static void main(String[] args) {
-		System.out.println("Hi tesing second project");
+		System.out.println("Hi tesing second project22");
 	}
 
 }
